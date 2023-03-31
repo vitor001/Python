0fa50f84febbd6696c71b3925a1e42bcef418eaf
vitor001 <38286628+vitor001@users.noreply.github.com>
2023-03-31 13:51:00 -0300

    Update mini game.py

--- mini game.py
+++ mini game.py
@@ -1,7 +1,7 @@
 palpite = 0
 numero = 89
 
-while True:  # executando o while antes da verificação como se fosse um do while
+while True:  
     palpite = int(input('Tente acertar o numero: '))
     if palpite == numero:  # verificação do código
         print('Parabens, voce acertou!')
