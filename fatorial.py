e5ca0cabda05da0d70c5998dfcf8609617578765
vitor001 <38286628+vitor001@users.noreply.github.com>
2023-03-31 13:53:00 -0300

    Update fatorial.py

--- fatorial.py
+++ fatorial.py
@@ -7,5 +7,5 @@ elif num == 0:
     print(f'O fatorial de {num} eh 1')
 else:
     for x in range(1, num + 1):
-        fatorial *= x  # mesma coisa que "fatorial = fatorial * x"
+        fatorial *= x 
     print(f'O valor do fatorial de {num} é: {fatorial}')
